23244c2623d9cbf5e5b173a488456dcc467af8ed
Anthony <alasam@live.com>
2021-11-10 11:00:33 -0500

    create generateRandomString function, update post urls.

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -39,3 +41,23 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  const longURL = req.body.longURL
+  const randomString = generateRandomString();
+  urlDatabase[randomString] = longURL;
+  console.log(urlDatabase);
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
+function generateRandomString() {
+  let result = '';
+  const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 62));
+  }
+  return result;
+}
+
+console.log(generateRandomString());
