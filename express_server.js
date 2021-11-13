ba2089d15a0f91860ce2baa0cfa5a0899df5423e
Anthony <alasam@live.com>
2021-11-12 19:03:13 -0500

    add username, submit, and logout option.

--- express_server.js
+++ express_server.js
@@ -2,14 +2,30 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(cookieParser());
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// Submitting username
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  console.log(req.cookies);
+  res.redirect(`/urls/`);
+});
+
+// Clear Cookie
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  console.log(req.cookies);
+  res.redirect(`/urls/`);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -28,19 +44,24 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"]
+    // ... any other vars
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  const templateVars = { shortURL, longURL: urlDatabase[shortURL] };
+  const templateVars = { shortURL, longURL: urlDatabase[shortURL], username: req.cookies["username"]};
   res.render("urls_show", templateVars);
 });
+
 
 // Submitting new long url
 app.post("/urls", (req, res) => {
@@ -60,10 +81,18 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL]
   res.redirect("/urls")
 });
+
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${shortURL}`); 
+})
 
 function generateRandomString() {
   const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -75,3 +104,4 @@ function generateRandomString() {
 
   return result;
 }
+
